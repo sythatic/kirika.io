dea5d7442d273ffb03104b5669d3d1522f5df251
Dom Panaia <sythatic@proton.me>
2025-03-12 13:11:41 -0400

    update

--- resources/hider.js
+++
@@ -1,8 +0,0 @@
-function setDisplay() {
-    var x = document.getElementById("hidden");
-    if (x.style.display === "table") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "table";
-    }
-}
